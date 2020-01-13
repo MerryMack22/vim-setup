e11153f94e38a0c88a139586597b4180af992bef
Joshua Robinson <joshuayrobinson@gmail.com>
2020-01-13 15:54:55 -0500

    Updated installer to check for ctags and install manually for ubuntu/debian

--- setupCustomVim.sh
+++ setupCustomVim.sh
@@ -4,19 +4,31 @@ RED='\033[0;31m]'
 YELLOW='\033[1;33m'
 NC='\033[0m' # no color
 
+function buildCtags {
+  echo -e "${RED}For better tags, manually compiling and installing ${NC} UNIVERSAL CTAGS ${YELLOW}:${NC} https://github.com/universal-ctags/ctags"
+  git clone https://github.com/universal-ctags/ctags ~/ctags
+  cd ~/ctags
+  git pull
+  ./autogen.sh
+  ./configure
+  make
+  sudo make install
+}
+
 function installZypper {
   echo -e "${YELLOW}Installing dependencies (zypper)${NC}"
-  sudo zypper install -y git wget
+  sudo zypper install -y git wget ctags gvim
 }
 
 function installYum {
   echo -e "${YELLOW}Installing dependencies (yum)${NC}"
-  sudo yum install -y git wget
+  sudo yum install -y git wget ctags
 }
 
 function installApt {
   echo -e "${YELLOW}Installing dependencies (apt)${NC}"
   sudo apt install -y git wget vim-gtk3
+  buildCtags
 }
 
 function installDependencies {
@@ -30,25 +42,30 @@ function installDependencies {
     installApt
     return 1
   else
-    echo -e "${RED}Please ensure both Git and wget are installed.${NC}"
+    echo -e "${RED}Please ensure git, wget, and ctags are installed.${NC}"
   fi
   exit 1
 }
 
 
 #verify dependencies are installed
-GIT_INSTALLED=true
-WGET_INSTALLED=true
+INSTALL_GIT=false
+INSTALL_WGET=false
+INSTALL_CTAGS=false
 
 if [ -z "$(command -v git)" ]; then
-  GIT_INSTALLED=false
+  INSTALL_GIT=true
 fi
 
 if [ -z "$(command -v wget)" ]; then
-  WGET_INSTALLED=false
+  INSTALL_WGET=true
 fi
 
-if [ ! $GIT_INSTALLED ] || [ ! $WGET_INSTALLED ]; then
+if [ -z "$(command -v ctags)" ]; then
+  INSTALL_CTAGS=true
+fi
+
+if [ $INSTALL_GIT ] || [ $WGET_INSTALLED ] || [ $CTAGS_INSTALLED ]; then
   installDependencies
 fi
 
